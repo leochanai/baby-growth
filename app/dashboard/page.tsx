70a6731a4329d21c994f1a6923549ad0a23cf53e
Kratos <geniusliuc@gmail.com>
2025-10-18 17:54:22 +0800

    Remove DataTable and SectionCards from Dashboard page; update layout with content utility classes

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,14 +1,12 @@
 import { AppSidebar } from "@/components/app-sidebar"
 import { ChartAreaInteractive } from "@/components/chart-area-interactive"
-import { DataTable } from "@/components/data-table"
-import { SectionCards } from "@/components/section-cards"
 import { SiteHeader } from "@/components/site-header"
 import {
   SidebarInset,
   SidebarProvider,
 } from "@/components/ui/sidebar"
 
-import data from "./data.json"
+// no table data (table removed)
 
 export default function Page() {
   return (
@@ -26,11 +24,9 @@ export default function Page() {
         <div className="flex flex-1 flex-col">
           <div className="@container/main flex flex-1 flex-col">
             <div className="content-y stack-y flex flex-col">
-              <SectionCards />
               <div className="content-x">
                 <ChartAreaInteractive />
               </div>
-              <DataTable data={data} />
             </div>
           </div>
         </div>
